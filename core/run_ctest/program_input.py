3dd46598e18316e4010160bf3ea5e1847dfc8c51
Jessica Yung-chieh Huang <43314345+jessicahuang523@users.noreply.github.com>
2022-11-10 17:02:46 -0600

    Update program_input.py

--- core/run_ctest/program_input.py
+++ core/run_ctest/program_input.py
@@ -3,12 +3,12 @@
 p_input = {
     # run mode
     "run_mode": "run_ctest", # string
-    # name of the project, i.e. hadoop-common, hadoop-hdfs, see constant.py
+    # name of the project, i.e. hadoop-common, hadoop-hdfs
     "project": "hadoop-common", # string
     # path to param -> tests json mapping
     "mapping_path": "../../data/ctest_mapping/opensource-hadoop-common.json", # string
     # input directory hosting configuration files to be test, target-project-format specific
-    "conf_file_dir": "hadoop-common", # string
+    "conf_file_dir": "sample-hadoop-common", # string
     # display the terminal output live, without saving any results
     "display_mode": False, # bool
     # whether to use mvn test or mvn surefire:test
