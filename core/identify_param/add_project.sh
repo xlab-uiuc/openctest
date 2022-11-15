dcb4f0f193142d0835a5f6fd12b10fd2edf5f9a5
yuanfei2 <yuanfei2@fa22-cs527-051.cs.illinois.edu>
2022-11-15 17:41:08 -0600

    add hadoop-distcp to identify_params

--- core/identify_param/add_project.sh
+++ core/identify_param/add_project.sh
@@ -11,6 +11,8 @@ function setup_hadoop() {
     mvn clean install -DskipTests
     cd $home_dir/hadoop-hdfs-project/hadoop-hdfs
     mvn package -DskipTests
+    cd $home_dir/hadoop-tools/hadoop-distcp
+    mvn clean install -DskipTests
 }
 
 function setup_hbase() {
