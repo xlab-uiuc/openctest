c52dd307135573f9c5169b57fe5d278485fa9518
ych10 <jessicahuang29@gmail.com>
2022-11-24 16:59:14 -0600

    change hudi-common-default.tsv and update value_generation.py

--- core/generate_value/value_generation.py
+++ core/generate_value/value_generation.py
@@ -25,6 +25,9 @@ def read_tsv(module):
     if module == "zookeeper-server":
         assert len(params) == 32
         return 32
+    elif module == "hudi-common":
+        assert len(params) == 28
+        return 28
     else:
         assert len(params) == 90
         return 90
@@ -105,6 +108,8 @@ def print_params(module):
     f = open(module + output, "w")
     if module == "zookeeper-server":
         assert len(params) == 32
+    elif module == "hudi-common":
+        assert len(params) == 28
     else:
         assert len(params) >= 90
     for param in params:
