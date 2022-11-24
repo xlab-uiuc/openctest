e2a91816438a6d74702ecd4adf953aa68a935fb3
Pinhao <pinhaog2@illinois.edu>
2022-11-23 20:37:04 -0600

    clean up for PR

--- core/identify_param/identify_param.sh
+++ core/identify_param/identify_param.sh
@@ -12,9 +12,9 @@ function main() {
       usage
     else
         case $project in
-            hadoop-common | hadoop-hdfs | hbase-server | zookeeper-server | alluxio-core) python3 runner.py $project; python3 collector.py $project ;;
+            hadoop-common | hadoop-hdfs | hbase-server | zookeeper-server | alluxio-core | cassandra) python3 runner.py $project; python3 collector.py $project ;;
             -h | --help) usage ;;
-            *) echo "Unexpected project: $project - only support hadoop-common, hadoop-hdfs, hbase-server, zookeeper-server and alluxio-core." ;;
+            *) echo "Unexpected project: $project - only support hadoop-common, hadoop-hdfs, hbase-server, zookeeper-server, alluxio-core, and cassandra." ;;
         esac
     fi
 }
