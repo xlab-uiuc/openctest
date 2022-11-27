7657e24b419298d0a8f613f334f14cf94696221d
Yuankai Wang <yuankai4@illinois.edu>
2022-11-26 18:19:09 -0600

    add basic kafka info

--- core/add_project.sh
+++ core/add_project.sh
@@ -48,6 +48,12 @@ function setup_alluxio() {
     mvn clean install -DskipTests -Dcheckstyle.skip -Dlicense.skip -Dfindbugs.skip -Dmaven.javadoc.skip=true
 }
 
+function setup_kafka() {
+    [ ! -d "app/ctest-kafka" ] && git clone https://github.com/wykkevin/kafka.git app/ctest-kafka
+    cd app/ctest-kafka
+    git fetch && git checkout ctest-injection
+}
+
 function usage() {
     echo "Usage: add_project.sh <main project>"
     exit 1
@@ -64,6 +70,7 @@ function main() {
             hbase) setup_hbase ;;
             zookeeper) setup_zookeeper ;;
             alluxio) setup_alluxio ;;
+            kafka) setup_kafka ;;
             *) echo "Unexpected project: $project - only support hadoop, hbase, zookeeper and alluxio." ;;
         esac
     fi
