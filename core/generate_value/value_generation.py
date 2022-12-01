6ecaaaae3057b063b59cce478facaa4113c6947e
yuanfei2 <yuanfei2@fa22-cs527-051.cs.illinois.edu>
2022-11-30 22:44:02 -0600

    update value_generation.py

--- core/generate_value/value_generation.py
+++ core/generate_value/value_generation.py
@@ -25,6 +25,9 @@ def read_tsv(module):
     if module == "zookeeper-server":
         assert len(params) == 32
         return 32
+    elif module == "hadoop-yarn-common":
+        assert len(params) == 574
+        return 574
     else:
         assert len(params) == 90
         return 90
@@ -105,6 +108,8 @@ def print_params(module):
     f = open(module + output, "w")
     if module == "zookeeper-server":
         assert len(params) == 32
+    elif module == "hadoop-yarn-common":
+        assert len(params) == 574
     else:
         assert len(params) >= 90
     for param in params:
