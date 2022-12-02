d50ccc6eb9fa736b46bd22e9eb6c0d020aec1bb0
KobeNorris <kobenorriswu@gmail.com>
2022-12-02 00:47:15 -0600

    Feat: Modify code to fit requirement

--- core/generate_value/value_generation.py
+++ core/generate_value/value_generation.py
@@ -25,6 +25,9 @@ def read_tsv(module):
     if module == "zookeeper-server":
         assert len(params) == 32
         return 32
+    elif module == "hadoop-yarn-tls":
+        assert len(params) == 222
+        return 16
     else:
         assert len(params) == 90
         return 90
@@ -105,6 +108,8 @@ def print_params(module):
     f = open(module + output, "w")
     if module == "zookeeper-server":
         assert len(params) == 32
+    elif module == "hadoop-yarn-tls":
+        assert len(params) == 222
     else:
         assert len(params) >= 90
     for param in params:
