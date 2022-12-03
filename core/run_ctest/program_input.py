f9e9c0d5d0ea24a36c4b84cc54dbd8c274c4e0e9
kfadillah <fadillahmichael0@gmail.com>
2022-12-03 12:00:38 -0600

    update script

--- core/run_ctest/program_input.py
+++ core/run_ctest/program_input.py
@@ -8,11 +8,11 @@ p_input = {
     # path to param -> tests json mapping
     "mapping_path": "../../data/ctest_mapping/opensource-redisson.json", # string
     # input directory hosting configuration files to be test, target-project-format specific
-    "conf_file_dir": "test_cassandra", # string
+    "conf_file_dir": "../default_configs/redisson-default.tsv", # string
     # display the terminal output live, without saving any results
-    "display_mode": False, # bool
+    "display_mode": True, # bool
     # whether to use mvn test or mvn surefire:test
-    "use_surefire": False, # bool
+    "use_surefire": True, # bool
     # additional maven options to pass to `mvn surefire:test -Dtest=...`
     "maven_args": ["-DredisBinary=/usr/bin/redis-server"], # list of strings, each element is an option
     # timeout on the mvn test command
