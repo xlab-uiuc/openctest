e366e49b1ac97dc6612256d06661e33576675418
Chen Song <chens3@illinois.edu>
2022-12-05 19:49:43 -0600

    adding test_result/kylin-common

--- core/generate_ctest/parse_input.py
+++ core/generate_ctest/parse_input.py
@@ -13,6 +13,7 @@ project = p_input["project"]
 def parse_value_file(path):
     """return param: [(value type, value)]"""
     data = {}
+    print(path)
     samples = [x.strip("\n").split("\t") for x in open(path)]
     for row in samples:
         parameter = row[0]
