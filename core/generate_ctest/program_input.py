2af7f3c6799d41ead80628ca1fe953f7ba332765
ConstaT99 <slyvestert@outlook.com>
2022-12-09 17:36:10 -0600

    clean program input file

--- core/generate_ctest/program_input.py
+++ core/generate_ctest/program_input.py
@@ -19,44 +19,6 @@ p_input = {
     "cmd_timeout": None, # int
 }
 
-# p_input = {
-#     # run mode
-#     "run_mode": "generate_ctest", # string
-#     # name of the project, i.e. hadoop-common, hadoop-hdfs, see constant.py
-#     "project": "kylin-storage", # string
-#     # path to param -> tests json mapping
-#     "mapping_path": "../../data/ctest_mapping/opensource-kylin-storage.json", # string
-#     # good values of params tests will be run against
-#     "param_value_tsv": "sample-kylin-storage.tsv", # string
-#     # display the terminal output live, without saving any results
-#     "display_mode": False, # bool
-#     # whether to use mvn test or mvn surefire:test
-#     "use_surefire": True, # bool
-#     # additional maven options to pass to `mvn surefire:test -Dtest=...`
-#     "maven_args": ["-DfailIfNoTests=false"], # list of strings, each element is an option
-#     # timeout on the mvn test command
-#     "cmd_timeout": None, # int
-# }
-
-# p_input = {
-#     # run mode
-#     "run_mode": "generate_ctest", # string
-#     # name of the project, i.e. hadoop-common, hadoop-hdfs, see constant.py
-#     "project": "kylin-tool", # string
-#     # path to param -> tests json mapping
-#     "mapping_path": "../../data/ctest_mapping/opensource-kylin-tool.json", # string
-#     # good values of params tests will be run against
-#     "param_value_tsv": "sample-kylin-tool.tsv", # string
-#     # display the terminal output live, without saving any results
-#     "display_mode": False, # bool
-#     # whether to use mvn test or mvn surefire:test
-#     "use_surefire": True, # bool
-#     # additional maven options to pass to `mvn surefire:test -Dtest=...`
-#     "maven_args": [], # list of strings, each element is an option
-#     # timeout on the mvn test command
-#     "cmd_timeout": None, # int
-# }
-
 assert p_input["project"] \
     and p_input["mapping_path"] \
     and p_input["param_value_tsv"], ">>>>[ctest_core] please specify input"
