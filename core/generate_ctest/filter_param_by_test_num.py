5b7ab120a94145423d520a65c8b30cac5b67d7b7
zixi2 <zixi2@fa22-cs527-110.cs.illinois.edu>
2022-12-10 20:10:35 -0600

    update hdfs-rbf

--- core/generate_ctest/filter_param_by_test_num.py
+++ core/generate_ctest/filter_param_by_test_num.py
@@ -2,7 +2,7 @@
 ## Fill free to modify min and max to what you need
 import json
 from program_input import p_input
-count = 30
+count = 100
 min = 0
 max = 32
 j = json.load(open("../../data/ctest_mapping/opensource-"+p_input["project"]+".json"))
