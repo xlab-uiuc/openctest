9aa9623a5fde9a01b1571b4c5419047111400086
sylvia <sylvia@MacBook-Pro-2.local>
2022-12-11 22:30:24 -0600

    Adding Druid (Druid/core module) to openctest

--- core/identify_param/add_project.sh
+++ core/identify_param/add_project.sh
@@ -47,6 +47,14 @@ function setup_alluxio() {
     mvn clean install -DskipTests -Dcheckstyle.skip -Dlicense.skip -Dfindbugs.skip -Dmaven.javadoc.skip=true
 }
 
+function setup_druid() {
+    [ ! -d "app/ctest-druid" ] && git clone https://github.com/b06902047/druid.git app/ctest-druid
+    cd app/ctest-druid
+    git fetch && git checkout ctest-core-logging-release
+    cd core
+    mvn install -DskipTests -Dcheckstyle.skip
+}
+
 function usage() {
     echo "Usage: add_project.sh <main project>"
     exit 1
@@ -63,7 +71,8 @@ function main() {
             hbase) setup_hbase ;;
             zookeeper) setup_zookeeper ;;
             alluxio) setup_alluxio ;;
-            *) echo "Unexpected project: $project - only support hadoop, hbase, zookeeper and alluxio." ;;
+            druid) setup_druid ;;
+            *) echo "Unexpected project: $project - only support hadoop, hbase, zookeeper, alluxio and druid." ;;
         esac
     fi
 }
